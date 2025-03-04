da35f411b702e8c32e2b804f4bd919102438f5e9
sunil jangid <matt348806@gmail.com>
2025-03-04 18:38:36 +0530

    new changes

--- src/components/profile/ExternalProfileInfo.tsx
+++ src/components/profile/ExternalProfileInfo.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { memo, useState } from "react";
+import { memo } from "react";
 import {
     AtSign,
     Check,
@@ -11,19 +11,19 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import Social from "./Social";
-import MessageModal from "./MessageModal";
 import { toast } from "@/hooks/use-toast";
 import { useUser } from "@/contexts/UserContext";
 import { User } from "@/types/user";
 import { redirect, usePathname } from "next/navigation";
 import ProfilePlaceholder from "@/media/user.png";
 import FollowDetails from "./FollowDetails";
+import MessageModal from "./MessageModal";
 
- function ExternalProfileInfo({ user }: { user: User }) {
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+function ExternalProfileInfo({ user }: { user: User }) {
     const { user: authUser } = useUser();
     const pathname = usePathname();
     if (pathname === "/profile" || authUser?._id === user._id) redirect("/profile");
+
     return (
         <div className="flex flex-col space-x-4 w-full relative items-center lg:items-start text-center lg:text-left space-y-4">
             <div className="w-20 h-20 lg:w-24 lg:h-24 relative -mt-12 lg:ml-4">
@@ -61,38 +61,17 @@ import FollowDetails from "./FollowDetails";
                     </div>
                 )}
                 <FollowDetails user={user} />
+                {user.profile?.availableForHire && (
+                    <div className="flex space-x-2 items-center text-green-500">
+                        <h2 className="text-lg font-semibold">Available for hire </h2>
+                        <Check />
+                    </div>
+                )}
+
+                {authUser && user && <MessageModal recepient={user} sender={authUser} />}
             </div>
 
-            {user.profile?.availableForHire && (
-                <div className="flex space-x-2 items-center text-green-500">
-                    <h2 className="text-lg font-semibold">Available for hire </h2>
-                    <Check />
-                </div>
-            )}
 
-            <Button
-                className="text-lg lg:w-5/6 md:w-2/4 rounded-full w-3/4"
-                onClick={() => {
-                    if (authUser) setIsModalOpen(true);
-                    else {
-                        toast({
-                            duration: 5000,
-                            title: "Invalid Action",
-                            description: "You need to login to send a message",
-                            variant: "destructive",
-                        });
-                    }
-                }}
-            >
-                <Mail /> Message
-            </Button>
-
-            <MessageModal
-                label={user.fullName}
-                isOpen={isModalOpen}
-                onClose={() => setIsModalOpen(false)}
-                onSubmit={() => setIsModalOpen(false)}
-            />
 
             <div className="!mt-8 w-[95%] p-4 lg:p-0">
                 <h2 className="text-muted-foreground font-bold uppercase">About</h2>
