b7b8652bed738aaf25ac2b2c31a216dade237074
Sunil Jangir <matt348806@gmail.com>
2025-03-15 09:33:48 +0530

    fixed an env leak

--- src/Providers/GoogleAuthWrapper.tsx
+++ src/Providers/GoogleAuthWrapper.tsx
@@ -3,8 +3,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 const GoogleAuthWrapper = ({ children }: { children: React.ReactNode }) => {
 
     const googleClientId: string =
-        process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ||
-        "298361459600-pe2mfqhml6qpji740893fke4r7646kh3.apps.googleusercontent.com";
+        process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 
     return <GoogleOAuthProvider clientId={googleClientId}> {children} </GoogleOAuthProvider>
         ;
